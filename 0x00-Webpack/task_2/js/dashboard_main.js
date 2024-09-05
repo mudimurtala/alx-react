c476e51f7844f68c2b4ce8f6606683efb4f4147b
mudimurtala <ibnmuhyideen95@gmail.com>
2024-09-05 20:30:45 +0100

    Add solutions to task-2

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,18 +1,19 @@
 import $ from 'jquery';
+import _ from 'lodash';
 import '../css/main.css';
+import '../assets/holberton-logo.jpg';
 
-$('body').append('<div id="logo"></div>');
+
+let count = 0;
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let count = 0;
-
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
+    count++;
+    $('#count').html(`${count} clicks on the button`);
 }
-
-$('button').on('click', _.debounce(updateCounter, 300));
+const debounced = _.debounce(updateCounter, 500);
+$('button').on('click', debounced);
