dcbf0d2dcd8d25573e834afcf7f149fc935c39ce
mudimurtala <ibnmuhyideen95@gmail.com>
2024-09-05 20:34:48 +0100

    Add solutions to task-2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,27 +1,36 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',  // Adjust this if you have multiple entries
-  output: {
-    filename: '[name].[contenthash].bundle.js',  // Use unique filenames
-    path: path.resolve(__dirname, 'public'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './js/dashboard_main.js')
+    }, 
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
     },
-  },
+    performance: {
+        hints: false,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
 };
